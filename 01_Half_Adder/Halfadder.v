5aa64e00d582d808077d70b52756281ac4d11e7e
Vinithkumar2007 <vinith21dhiya@gmail.com>
2026-06-13 21:39:52 +0530

    Update Halfadder.v

--- 01_Half_Adder/Halfadder.v
+++ 01_Half_Adder/Halfadder.v
@@ -1,1 +1,7 @@
+// Half Adder - Dataflow Modeling
+// Tools: Icarus Verilog, GTKWave
 
+module half_adder(input a, b, output sum, carry);
+  assign sum = a ^ b;    // XOR for Sum
+  assign carry = a & b;  // AND for Carry
+endmodule
