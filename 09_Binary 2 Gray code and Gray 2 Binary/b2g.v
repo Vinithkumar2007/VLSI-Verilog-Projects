3cf387e597a64d79f9bb9689fd93cc1464d7bdc5
VINITHKUMAR K <vinith21dhiya@gmail.com>
2026-06-17 21:49:57 +0530

    Add files via upload

--- 09_Binary 2 Gray code and Gray 2 Binary/b2g.v
+++ 09_Binary 2 Gray code and Gray 2 Binary/b2g.v
@@ -1,1 +1,10 @@
+module b2g(
+    input b0, b1, b2, b3,
+    output g0, g1, g2, g3
+);
 
+assign g3 = b3;
+assign g2 = b3 ^ b2;
+assign g1 = b2 ^ b1;
+assign g0 = b1 ^ b0;
+endmodule
---
+++ 09_Binary 2 Gray code and Gray 2 Binary/g2b.v
@@ -0,0 +1,9 @@
+module g2b(
+    input g0, g1, g2, g3,
+    output b0, b1, b2, b3
+);
+assign b3 = g3;
+assign b2 = g3 ^ g2;
+assign b1 = g3 ^ g2 ^ g1;
+assign b0 = g3 ^ g2 ^ g1 ^ g0;
+endmodule
